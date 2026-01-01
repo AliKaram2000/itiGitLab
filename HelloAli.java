22b93cc7cc0aa6ea6c4ee77e19f75d53fc596384
aeinae <aliabdalkareem.ak2000@gmail.com>
2026-01-01 10:39:51 +0200

    Added a comment on changes

--- HelloAli.java
+++ HelloAli.java
@@ -4,7 +4,7 @@ public class HelloAli{
 	public static void main(String[] args){
 		
 		System.out.println("HelloAli");
-		System.out.println("Hello");
+		System.out.println("Hello"); // adding a comment
 		
 	}
 }
